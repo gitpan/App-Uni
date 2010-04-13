9c6b93a3b3dac0c07e6fabb2686b8cd7ece92daf
☻ 唐鳳 ☺ <cpan@audreyt.org>
2010-04-13 13:53:20 +0000

    Import of AUDREYT/App-Uni-v0.12.0 from CPAN.
    gitpan-cpan-distribution: App-Uni
    gitpan-cpan-version:      v0.12.0
    gitpan-cpan-path:         AUDREYT/App-Uni-v0.12.0.tar.gz
    gitpan-cpan-author:       AUDREYT
    gitpan-cpan-maturity:     released

--- lib/App/Uni.pm
+++ lib/App/Uni.pm
@@ -1,30 +1,27 @@
-#!/usr/bin/perl
-package App::Uni;
-use 5.008;
-use strict;
-use warnings;
-use constant SOURCE => 'unicore/UnicodeData.txt';
-our $VERSION = '0.02';
+use v5.12.0;
+package App::Uni v0.12.0;
+use open ':std' => ':utf8';
 
 sub main {
-    my ($file) = grep -f, map { "$_/".SOURCE } @INC
-        or die "Cannot find: ".SOURCE.$/;
+    my ($file) = grep { -f and -r }
+                  map { "$_/unicore/UnicodeData.txt" } @INC
+        or die 'Cannot find UnicodeData.txt in @INC';
 
-    binmode STDOUT, ':utf8';
-    open my $fh, '<', $file
-        or die "Cannot open $file: $!$/";
+    utf8::decode(
+        my $regex = join(' ', @_)
+    );
 
-    my $regex = join ' ', @_;
-
-    while (<$fh>) {
-        (/$regex/io and /(\w+);([^;]+)/) or next;
-
-        my ($code, $name) = ($1, $2);
-        ($name =~ /$regex/io or $code =~ /$regex/io) or next;
-
-        print $code, ' ', chr hex $code, ' ', $name, $/;
+    if (length $regex == 1) {
+        $regex = sprintf('(?:%s|%04X)', $regex, ord $regex);
     }
 
+    open my $fh, '<:mmap', $file;
+    for (<$fh>) {
+        if (/$regex/io and my ($code, $name) = /(\w+);([^;]+)/) {
+            say $code, ' ', chr hex $code, ' ', $name
+                if [$name, $code] ~~ /$regex/io;
+        }
+    }
     close $fh;
 }
 
@@ -40,13 +37,16 @@ App::Uni - Command-line utility to grep UnicodeData.txt
 
 =head1 VERSION
 
-This document describes version 0.02 of App::Uni, released December 10, 2009.
+This document describes version v0.12.0 of App::Uni, released April 13, 2010.
 
 =head1 SYNOPSIS
 
-    $ uni smiling
+    $ uni smiling face
     263A ☺ WHITE SMILING FACE
     263B ☻ BLACK SMILING FACE
+
+    $ uni ☺
+    263A ☺ WHITE SMILING FACE
 
 =head1 DESCRIPTION
 
@@ -56,6 +56,9 @@ the Unicode database included in the current Perl 5 installation.
 The arguments to the F<uni> program are joined with space and interpreted
 as a regular expression.  Character codes or names matching the regex
 (case-insensitively) are then printed out.
+
+If the argument is a single character, then the character itself is also
+printed out in addition to code and name matches.
 
 =head1 ACKNOWLEDGEMENTS
 
